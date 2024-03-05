93e0f52ab829c3c91ec877db2cfecfec6414f2c8
Huma-Mohsincls <you@example.com>
2024-03-05 09:47:07 +0500

    first commit

---
+++ question-3/index.js
@@ -0,0 +1,3 @@
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+var fav_num = 99;
+console.log("Allah has ".concat(fav_num, " names."));
---
+++ question-3/index.ts
@@ -0,0 +1,5 @@
+
+// Question 9: Favorite Number: Store your favorite number in a variable. Then, create a message revealing your favorite number. Print that message.
+
+let fav_num:number=99;
+console.log(`Allah has ${fav_num} names.`)
